85e5e136c1e44730973d8323d7dedadeaf9f242d
Sam Rabin <samrabin@ucar.edu>
2025-05-21 15:08:25 -0600

    Fix/update testing.sh.

--- doc/testing.sh
+++ doc/testing.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -x
 
 rm -rf _build _publish
 doc-builder/build_docs_all_versions
@@ -7,6 +8,10 @@ doc-builder/build_docs_all_versions
 rm -rf _build_docker
 ./build_docs -r _build_docker -v latest -d -c
 
-rm -rf _build_docker; ./build_docs -b _build_docker -d -c -v latest
+rm -rf _build_docker
+./build_docs -b _build_docker -d -c
+
+cd doc-builder/test
+make test
 
 exit 0
