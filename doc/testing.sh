ea4b6d27c3b5660f5154e5c9eff219778a31b23b
Sam Rabin <samrabin@ucar.edu>
2025-05-22 14:53:36 -0600

    more testing.sh changes

--- doc/testing.sh
+++ doc/testing.sh
@@ -2,20 +2,22 @@
 set -e
 set -x
 
+rm -rf _publish*
 
 # Build all docs using container
-rm -rf _build _publish*
-./build_docs_all_versions -d --site-root "$PWD/_publish"
+rm -rf _build _publish
+d1="$PWD/_publish_container"
+./build_docs_all_versions -d --site-root "$d1"
 mv _publish _publish_container
 
-# # Build all docs using ctsm_pylib
-# rm -rf _build _publish
-# d2="$PWD/_publish_nocontainer"
-# conda run -n ctsm_pylib ./build_docs_all_versions --site-root "$d2"
-# mv _publish "${d2}"
+# Build all docs using ctsm_pylib
+rm -rf _build _publish
+d2="$PWD/_publish_nocontainer"
+conda run -n ctsm_pylib ./build_docs_all_versions --site-root "$d2"
+mv _publish "${d2}"
 
-# # Make sure container version is identical to no-container version
-# diff -qr "${d1}" "${d2}"
+# Make sure container version is identical to no-container version
+diff -qr "${d1}" "${d2}"
 
 # # Check that -r -v works
 # rm -rf _build_docker
