603afb1ec77182807a6645ed2bafda64e3221aa5
Sam Rabin <samrabin@ucar.edu>
2025-05-22 14:16:45 -0600

    build_docs_all_versions: Add -d option.

--- doc/testing.sh
+++ doc/testing.sh
@@ -2,16 +2,28 @@
 set -e
 set -x
 
-rm -rf _build _publish
-./build_docs_all_versions
 
-rm -rf _build_docker
-./build_docs -r _build_docker -v latest -d -c
+# Build all docs using container
+rm -rf _build _publish_container
+./build_docs_all_versions -d
 
-rm -rf _build_docker
-./build_docs -b _build_docker -d -c
+# Build all docs using ctsm_pylib
+rm -rf _build _publish_nocontainer
+conda run -n ctsm_pylib ./build_docs_all_versions
 
-cd doc-builder/test
-make test
+# # Make sure container version is identical to no-container version
+# diff -qr _publish_container/ _publish_nocontainer/
+
+# # Check that -r -v works
+# rm -rf _build_docker
+# ./build_docs -r _build_docker -v latest -d -c
+
+# # Check that -b works
+# rm -rf _build_docker
+# ./build_docs -b _build_docker -d -c
+
+# # Check that pure Makefile method works
+# cd doc-builder/test
+# conda run -n ctsm_pylib make test
 
 exit 0
