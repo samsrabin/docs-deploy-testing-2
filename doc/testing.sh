fb0943aac96657cd6047314b5ca2a0372834205e
Sam Rabin <samrabin@ucar.edu>
2025-05-22 15:03:58 -0600

    Final testing.sh changes.

--- doc/testing.sh
+++ doc/testing.sh
@@ -8,29 +8,29 @@ rm -rf _publish*
 rm -rf _build _publish
 d1="$PWD/_publish_container"
 ./build_docs_all_versions -d --site-root "$PWD/_publish"
-# VERSION LINKS WILL NOT RESOLVE BECAUSE OF THIS MV
-mv _publish _publish_container
+# VERSION LINKS WILL NOT RESOLVE IN _publish_container
+cp _publish _publish_container
 
 # Build all docs using ctsm_pylib
 rm -rf _build _publish
 d2="$PWD/_publish_nocontainer"
 conda run -n ctsm_pylib ./build_docs_all_versions --site-root "$PWD/_publish"
-# VERSION LINKS WILL NOT RESOLVE BECAUSE OF THIS MV
-mv _publish "${d2}"
+# VERSION LINKS WILL NOT RESOLVE IN _publish_nocontainer
+cp _publish "${d2}"
 
 # Make sure container version is identical to no-container version
 diff -qr "${d1}" "${d2}"
 
-# # Check that -r -v works
-# rm -rf _build_docker
-# ./build_docs -r _build_docker -v latest -d -c
+# Check that -r -v works
+rm -rf _build_docker
+./build_docs -r _build_docker -v latest -d -c
 
-# # Check that -b works
-# rm -rf _build_docker
-# ./build_docs -b _build_docker -d -c
+# Check that -b works
+rm -rf _build_docker
+./build_docs -b _build_docker -d -c
 
-# # Check that pure Makefile method works
-# cd doc-builder/test
-# conda run -n ctsm_pylib make test
+# Check that doc-builder tests pass
+cd doc-builder/test
+conda run -n ctsm_pylib make test
 
 exit 0
